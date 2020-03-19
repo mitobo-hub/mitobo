4ca5eb4601027d6352618419be3c76a3fb73077c
Birgit Moeller <birgit.moeller@informatik.uni-halle.de>
2020-03-19 15:34:22 +0100

    Fixed bug in case of start or end point lying outside of image domain: no more clipping is done, but alternative intersection points are calculated; changed type of color parameter.

--- src/main/java/de/unihalle/informatik/MiToBo/visualization/drawing/DrawLine.java
+++ src/main/java/de/unihalle/informatik/MiToBo/visualization/drawing/DrawLine.java
@@ -24,6 +24,7 @@
 
 package de.unihalle.informatik.MiToBo.visualization.drawing;
 
+import java.awt.Color;
 import java.awt.geom.Point2D;
 
 import de.unihalle.informatik.Alida.annotations.ALDAOperator;
@@ -36,8 +37,8 @@ import de.unihalle.informatik.MiToBo.core.operator.MTBOperator;
 
 
 /**
- * Implementation of the Bresenham line drawing algorithm according to <br\>
- * http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+ * Implementation of the Bresenham line drawing algorithm according to
+ * <a href="http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm">Wikipedia</a>.
  * 
  * @author glass
  *
@@ -45,17 +46,22 @@ import de.unihalle.informatik.MiToBo.core.operator.MTBOperator;
 @ALDAOperator(genericExecutionMode=ALDAOperator.ExecutionMode.ALL, level=Level.APPLICATION)
 public class DrawLine extends MTBOperator
 {
-	@Parameter(label = "inImg", required = true, direction = Parameter.Direction.IN, supplemental = false, description = "input image", dataIOOrder = 0)
+	@Parameter(label = "inImg", required = true, direction = Parameter.Direction.INOUT, 
+			supplemental = false, description = "input image", dataIOOrder = 0)
 	private transient MTBImage inImg = null;
 	
-	@Parameter(label = "start", required = true, direction = Parameter.Direction.IN, supplemental = false, description = "starting point of the line", dataIOOrder = 1)
+	@Parameter(label = "start", required = true, direction = Parameter.Direction.IN, 
+			supplemental = false, description = "starting point of the line", dataIOOrder = 1)
 	private Point2D.Double start = null;
 	
-	@Parameter(label = "end", required = true, direction = Parameter.Direction.IN, supplemental = false, description = "end point of the line", dataIOOrder = 2)
+	@Parameter(label = "end", required = true, direction = Parameter.Direction.IN, 
+			supplemental = false, description = "end point of the line", dataIOOrder = 2)
 	private Point2D.Double end = null;
 	
-	@Parameter(label = "color", required = false, direction = Parameter.Direction.IN, supplemental = false, description = "color value to draw the line in", dataIOOrder = 3)
-	private Double color = 255.0;
+	@Parameter(label = "color", required = false, direction = Parameter.Direction.IN, 
+			supplemental = false, description = "color value for drawing the line, value rINT:gINT:bINT", 
+			dataIOOrder = 3)
+	private Color color = Color.white;
 	
 	public DrawLine() throws ALDOperatorException
 	{
@@ -79,8 +85,9 @@ public class DrawLine extends MTBOperator
 		inImg.updateAndRepaintWindow();
 	}
 	
-	
-	
+	/**
+	 * Draw line into image. 
+	 */
 	private void line()
 	{
 		int x0 = (int)start.x;
@@ -92,6 +99,89 @@ public class DrawLine extends MTBOperator
 		int sizeX = inImg.getSizeX();
 		int sizeY = inImg.getSizeY();
 		
+		// start and end points of line
+		double startX = x0;
+		double startY = y0;
+		double endX = x1;
+		double endY = y1;
+		
+		// direction of line
+		double rx = endX - startX;
+		double ry = endY - startY;
+		
+		/*
+		 * calculate intersections with image boundaries
+		 */
+				
+		// left boundary
+		double l_y = startY - startX/rx * ry;
+		
+		// right boundary
+		double r_y = startY + (sizeX-1 - startX)/rx * ry;
+		
+		// top boundary
+		double t_x = startX - startY/ry * rx;
+		
+		// bottom boundary
+		double b_x = startX + (sizeY-1 - startY)/ry * rx;
+	
+		/*
+		 * check intersection on left side, if outside image domain, 
+		 * use top or bottom intersection as start point for line...
+		 */
+		
+		if (l_y < 0) {
+			// line does not cross valid image area
+			if (t_x < 0 || t_x >= sizeX)
+				return;
+			
+			// line crosses top boundary of image
+			x0 = (int)(t_x + 0.5);
+			y0 = 0;
+		}
+		else if (l_y >= sizeY) {
+			// line does not cross valid image area
+			if (b_x < 0 || b_x >= sizeX)
+				return;
+			
+			// line crosses bottom boundary of image
+			x0 = (int)(b_x + 0.5);
+			y0 = sizeY-1;			
+		}
+		else {
+			x0 = 0;
+			y0 = (int)(l_y+0.5);			
+		}
+		
+		/*
+		 * check intersection on right side, if outside image domain, 
+		 * use top or bottom intersection as end point for line...
+		 */
+
+		if (r_y < 0) {
+			// line does not cross valid image area
+			if (t_x < 0 || t_x >= sizeX)
+				return;
+			
+			// line crosses top boundary of image
+			x1 = (int)(t_x + 0.5);
+			y1 = 0;
+		}
+		else if (r_y >= sizeY) {
+			// line does not cross valid image area
+			if (b_x < 0 || b_x >= sizeX)
+				return;
+			
+			// line crosses bottom boundary of image
+			x1 = (int)(b_x + 0.5);
+			y1 = sizeY-1;			
+		}
+		else {
+			x1 = sizeX-1;
+			y1 = (int)(r_y + 0.5);			
+		}
+				
+		// safety checks just in case that rounding produced invalid values
 		if(x0 < 0)
 		{
 			x0 = 0;
@@ -127,16 +217,18 @@ public class DrawLine extends MTBOperator
 		{
 			y1 = sizeY - 1;
 		}
-		
+
 		int dx = Math.abs(x1 - x0);
 		int dy = -Math.abs(y1 - y0);
 		int sx = x0 < x1 ? 1 : -1;
 		int sy = y0 < y1 ? 1 : -1; 
 		int err = dx + dy, e2; 
 		 
+		int iColor = ((color.getRed() & 0xff)<<16) + ((color.getGreen() & 0xff)<<8) 
+				+ ((color.getBlue() & 0xff));
 		for(;;)
 		{
-			inImg.putValueDouble(x0, y0, color);
+			inImg.putValueDouble(x0, y0, iColor);
 		    
 			if(x0 == x1 && y0 == y1)
 		    {
@@ -159,9 +251,11 @@ public class DrawLine extends MTBOperator
 		}
 	}
 	
-	
-	
-	public void setColor(double c)
+	/**
+	 * Specify color to use for drawing.
+	 * @param c	Color to use for drawing the line.
+	 */
+	public void setColor(Color c)
 	{
 		this.color = c;
 	}
